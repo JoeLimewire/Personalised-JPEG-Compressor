1784047e05fa703f7254a7aa26b7bdfbaef4da65
hobomojoe <44262938+hobomojoe@users.noreply.github.com>
2019-11-22 00:44:51 +0000

    RGB to YCbCr

--- Website/scripts/indexScript.js
+++ Website/scripts/indexScript.js
@@ -2,10 +2,9 @@ window.onload =function(){
     var i = document.getElementsByClassName("image");
 
     initSocket();
-
     //createTable();
-    canvas()
-
+    var data = getImgData();
+    rgbToYCbCr(data);
 
 }
 
@@ -44,7 +43,7 @@ function createTable(){
   }
 }
 
-function canvas(){
+function getImgData(){
 
   var canvas = document.getElementById("preview");
   var ctx = canvas.getContext("2d");
@@ -59,12 +58,11 @@ function canvas(){
   canvas.width = img.width;
   canvas.height = img.height;
 
-
-
   //put image on canvas
   ctx.drawImage(img,0,0,canvas.width,canvas.height);
   imgdata = ctx.getImageData(0,0,canvas.width,canvas.height);
-  console.log(imgdata);
+  //console.log(imgdata);
+
 
   for(var i = 0; i < imgdata.data.length; i= i +2){
     //imgdata.data[i] = imgdata.data[i] -200;
@@ -72,12 +70,54 @@ function canvas(){
  //PUT IMAGE DATA ON CANVAS
   ctx.putImageData(imgdata,0,0);
 
-  drawGrid(ctx);
+
+  //Function to draw a 8x8 grid over the canvas
+  //drawGrid(ctx);
 
   ctx.moveTo(20, 20);
   ctx.lineTo(40,40);
 
+  return imgdata;
 
+}
+
+function rgbToYCbCr(data){
+
+  /*
+  Grabs values of the image off of the CANVAS
+  Each pixel has 4 values (RGBA)
+  data[0] = R
+  data[1] = G
+  data[2] = B
+  data[3] = A
+  etc...
+
+  https://sistenix.com/rgb2ycbcr.html
+  */
+
+  var arr = new Array();
+
+  for(var i = 0; i < imgdata.data.length; i = i + 4){
+    //RGB values are the same
+    var r = data[i];
+    var g = data[i+1];
+    var b = data[i+2];
+
+    var Y = 16 + (65.738*r)/256 + (129.057*g)/256 + (25.064*b)/256;
+    var Cb = 128 - (37.945*r)/256 + (74.494*g)/256 + (112.439*b)/256;
+    var Cr = 128 + (112.439*r)/256 + (94.154*g)/256 + (18.285*b)/256;
+
+    //change float to int
+    arr[i,0] = parseInt(Y);
+    arr[i,1] = Cb;
+    arr[i,2] = Cr;
+
+    data[i] = Math.floor(Y);
+    data[i+1] = parseInt(Cb);
+    data[i+2] = parseInt(Cr);
+
+  }
+  console.log(arr[0,0]);
 }
 
 function drawGrid(ctx){
