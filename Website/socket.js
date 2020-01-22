3a0d3be6557449b4f0a35318d8747e387b46003e
hobomojoe <Hobomojoe1999@gmail.com>
2020-01-22 16:54:42 +0000

    Webpack

--- Website/socket.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var connect = require('connect');
-var app = express();
-var server = app.listen(5001,function(){
-  console.log("listening to port 5001");
-});
-
-//static files
-app.use(express.static('../Website'));
---
+++ Website2/Server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var connect = require('connect');
+var app = express();
+var server = app.listen(5001,function(){
+  console.log("listening to port 5001");
+});
+
+//static files
+app.use(express.static('../Website'));
+
+var dct = require('dct'), signal = [1,1,1,1,1];
+
+var coef = dct(signal);
+
+console.log(coef); // [12, 0, 0, 0, 0]
