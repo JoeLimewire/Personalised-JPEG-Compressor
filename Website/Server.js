4444514ae6fb481d8638868548e09560151be7ac
hobomojoe <Hobomojoe1999@gmail.com>
2020-02-11 19:32:12 +0000

    Almost Done
    Project Is almost finished

--- Website/Server.js
+++ Website/Server.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var connect = require('connect');
 var app = express();
-var server = app.listen(5001,function(){
-  console.log("listening to port 5001");
+var server = app.listen(5001, function() {
+    console.log("listening to port 5001");
 });
 
 //static files
-app.use(express.static('../Website'));
+app.use(express.static('public'));
 
-var dct = require('dct'), signal = [1,1,1,1,1];
+var dct = require('dct'),
+    signal = [1, 1, 1, 1, 1];
 
 var coef = dct(signal);
 
